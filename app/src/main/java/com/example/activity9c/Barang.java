aca234b9d3ab67a29a5c2796a7260ab8620784ae
Husna Amalia <amalia.husna.ft19@mail.umy.ac.id>
2021-06-18 14:08:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity9c/Barang.java
@@ -0,0 +1,36 @@
+package com.example.activity9c;
+
+import java.io.Serializable;
+
+public class Barang implements Serializable {
+    private String kode,nama;
+
+    public Barang(){}
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kd) {
+        this.kode = kd;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nm) {
+        this.nama = nm;
+    }
+
+    @Override
+    public String toString() {
+        return " " + kode + '\n' + " " + nama;
+    }
+
+    public Barang(String kd,String nm){
+        kode = kd;
+        nama = nm;
+    }
+}
+
